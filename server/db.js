ac12b7209c73dfed0334fae1d465d9bdad382e43
Abdullah Ehsan <abdullahehsan4242@gmail.com>
2024-04-04 22:12:11 +0500

    Revert "initial_commit"
    This reverts commit 84f9263316a8c469844907a6b46d6779779561db.

--- server/db.js
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose'
-const mongoConnectionURL = ''
-
-export const mongo = await mongoose.connect(mongoConnectionURL, {
-    dbName: "scanner24"
-})
-console.log("mongo connected")
